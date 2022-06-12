56847b9480f162d37dbe9a18584a9429a002c0c3
IvanPratasevich <iv.pratasevich@gmail.com>
2022-06-12 23:46:46 +0300

    feat: add message when source file does not exist

--- src/fs/mv.js
+++ src/fs/mv.js
@@ -2,14 +2,20 @@ import { createWriteStream, createReadStream } from 'node:fs';
 import { sep, resolve } from 'node:path';
 import { pipeline } from 'node:stream/promises';
 import { unlink } from 'node:fs/promises';
+import { exists } from '../additions/additions.js';
 
 export const mv = async (parameters) => {
   try {
     const pathToFile = resolve(parameters[0]);
     const sourceFile = pathToFile.split(sep).pop();
     const pathToDest = resolve(parameters[1], sourceFile);
-    const readStream = createReadStream(pathToFile);
-    const writeStream = createWriteStream(pathToDest);
+    const isSourceFileExists = await exists(pathToFile);
+    if (!isSourceFileExists) {
+      console.log('There is no file in directory!');
+      return;
+    }
+    const readStream = createReadStream(pathToFile, { flags: 'r' });
+    const writeStream = createWriteStream(pathToDest, { flags: 'wx' });
     await pipeline(readStream, writeStream);
     await unlink(pathToFile);
   } catch (error) {
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,11 +1,16 @@
 import { createReadStream } from 'node:fs';
 import { resolve } from 'node:path';
+import { exists } from '../additions/additions.js';
 
 export const calculateHash = async (parameters) => {
   try {
     const { createHash } = await import('crypto');
     const pathToSourceFile = resolve(parameters[0]);
     const hash = createHash('sha256');
+    const isSourceFileExists = await exists(pathToSourceFile);
+    if (!isSourceFileExists) {
+      return 'There is no file in directory!\nOperation failed!';
+    }
     const readStream = createReadStream(pathToSourceFile);
     return new Promise((resolve) => {
       readStream.on('data', (data) => hash.update(data));
